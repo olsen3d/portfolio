c4fbab79806c094ec568d3e8ab19eb738d986854
olsen3d <olsen3d@gmail.com>
2021-01-06 22:38:02 -0500

    express

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 8000
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '/index.html')))
+
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`)
+})
