13123996c9e29ad50c55dcd653c6af22daf39b2f
olsen3d <olsen3d@gmail.com>
2021-01-07 23:16:49 -0500

    work

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 8000
 
 app.use(express.static(__dirname));
 
+app.get('/animations', (req, res, next) => res.sendFile(path.join(__dirname, '/animations.html')))
+
+app.get('/archviz', (req, res, next) => res.sendFile(path.join(__dirname, '/archviz.html')))
+
 app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '/index.html')))
 
 app.listen(port, () => {
