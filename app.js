752f108525bd9e5c239dca883a87c565131f719c
olsen3d <olsen3d@gmail.com>
2021-01-09 00:06:44 -0500

    web dev section

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ function onVrViewLoad() {
   var vrView = new VRView.Player('#vrview', {
     image: 'https://www.olsen.dev/360',
     width: '100%',
-    height: '500px'
+    height: '600px',
+    is_stereo: false,
   });
 }
