2c4ae7782b0dd3be10a0804198a038feb33e56ea
olsen3d <olsen3d@gmail.com>
2021-01-09 23:26:12 -0500

    about

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-  var controller = new ScrollMagic.Controller({
-    globalSceneOptions: {
-      triggerHook: 'onLeave',
-      duration: "200%"
-    }
-  });
-
-  // get all slides
-  var slides = document.querySelectorAll("section.panel");
-
-  // create scene for every slide
-  for (var i=0; i<slides.length; i++) {
-    new ScrollMagic.Scene({
-        triggerElement: slides[i]
-      })
-      .setPin(slides[i], {pushFollowers: false})
-      .addIndicators() // add indicators (requires plugin)
-      .addTo(controller);
-  }
-})
-
 window.addEventListener('load', onVrViewLoad);
 
 function onVrViewLoad() {
-  // Selector '#vrview' finds element with id 'vrview'.
   var vrView = new VRView.Player('#vrview', {
     image: 'https://www.olsen.dev/360',
     width: '100%',
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors())
 
 app.use(express.static(__dirname));
 
-app.get('/webdev', (req, res, next) => res.sendFile(path.join(__dirname, '/webdev.html')))
+app.get('/softwaredev', (req, res, next) => res.sendFile(path.join(__dirname, '/softwaredev.html')))
 app.get('/animations', (req, res, next) => res.sendFile(path.join(__dirname, '/animations.html')))
 app.get('/archviz', (req, res, next) => res.sendFile(path.join(__dirname, '/archviz.html')))
 
